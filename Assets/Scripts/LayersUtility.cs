fc9d23a972a202c8db68027a14a576cfc9f12084
Sasha petrowski <sasha.petrowski@gmail.com>
2024-01-15 14:31:23 +0100

    Proto Scene & Layers

---
+++ Assets/Scripts/LayersUtility.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class Layers
+{
+    public readonly static int Default = 1 << 0;
+    public readonly static int Everything = ~0;
+
+    public readonly static int TransparentFX = 1 << 1;
+    public readonly static int IgnoreRaycast = 1 << 2;
+    public readonly static int Player = 1 << 3;
+    public readonly static int Water = 1 << 4;
+    public readonly static int UI = 1 << 5;
+    public readonly static int Terrain = 1 << 6;
+}
+
+public static class LayersUtility
+{
+
+}
